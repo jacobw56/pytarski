1d491f48608867d78995be3cd26c8de27aa6a498
jacobw56 <jacobw56@gmail.com>
2023-11-03 16:23:01 -0400

    fixed typos in boole example

--- examples/boole_test.py
+++ examples/boole_test.py
@@ -8,15 +8,15 @@ tt = tw.BooleTruthTable('not p and q')
 l = [{'p': False, 'q': True}]
 
 # check if all the dictionaries contain scenarios that are true
-tt.checkTrue(l) # should be True
+print(tt.checkTrue(l)) # should be True
 
 # if you add a scenario that doesn't work, checkTrue will return False
-l = l.append({'p': False, 'q': False})
-tt.checkTrue(l) # should be False
+l.append({'p': False, 'q': False})
+print(tt.checkTrue(l)) # should be False
 
 # likewise we can create a list that covers some or all of the false scenarios
 m = [{'p': True, 'q': True}, {'p': True, 'q': False}, {'p': False, 'q': False}]
 
 # and we can evaluate this list to check if they are all False, in which case 
 # checkFalse will somewhat confusingly return True
-tt.checkFalse(m)
+print(tt.checkFalse(m)) # should be True
