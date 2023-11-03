b73096cb559acf02b62d2e14fb7848f90535b231
jacobw56 <jacobw56@gmail.com>
2023-11-03 16:21:39 -0400

    Updated to version 0.0.6

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.4' 
+VERSION = '0.0.6' 
 DESCRIPTION = 'Tarski\'s World for python'
 LONG_DESCRIPTION = 'A python version of the Tarski\'s World universe'
 
